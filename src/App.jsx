7abef7321030ea8f38f520b4d2765792ed7d0c41
mavishal1648 <mavishal1648@gmail.com>
2024-10-31 12:02:46 +0530

    created pagesfolder and routes for the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signin from "./Pages/signin";
+import Signout from "./Pages/Signup";
+import About from "./Pages/about";
+import Profile from "./Pages/Profile";
+import Signup from "./Pages/Signup";
 export default function App() {
   return (
-    <div className='text-rose-300 font-bold underline'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
