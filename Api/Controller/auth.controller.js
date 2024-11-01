83517881ae5f678d583f882a4977f6aeccd20bdc
mavishal1648 <mavishal1648@gmail.com>
2024-11-01 11:53:51 +0530

    ErrorHandler

--- Api/Controller/auth.controller.js
+++ Api/Controller/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../Models/User.Model.js";
 import bcrypt from 'bcrypt'
 
-export const signUp = async(req,res)=>{
+export const signUp = async(req,res,next)=>{
     const{email,username,password} = req.body;
     const hashPassword = bcrypt.hashSync(password,10);
     const newUser = new User({username,email,password:hashPassword});
@@ -9,6 +9,6 @@ export const signUp = async(req,res)=>{
         await newUser.save();
         res.status(201).send("User Created Successfully")
     }catch(err){
-        res.status(500).json(err.message);
+        next(err);
     }
 }
--- Api/index.js
+++ Api/index.js
@@ -17,6 +17,16 @@ app.use(express.json())
 
 app.use("/api/user/",UserRouter);
 app.use("/api/auth/",authRouter);
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const errMessage = err.message || "Internal Server Issue"
+    return res.status(statusCode).json({
+        error:true,
+        statusCode,
+        message:errMessage
+    });
+});
+
 
 app.listen(3100,()=>{
     console.log("Server is listening on port 3100!");
